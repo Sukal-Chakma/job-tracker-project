65369bf7d4bf6b44c36e1c3ad7cb68254789b672
SukalChakma-programmer <sukal7638@gmail.com>
2026-02-24 18:56:29 +0600

    btn toggle is completed

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,19 @@
 console.log('connect')
+// empty array for value get interview and rejected
+let interviewList = [];
+let rejectedList = [];
+let currentList = [];
+let currentStatus = 'all-btn';
+
+let total = document.getElementById("total");
+let interview = document.getElementById("interview");
+let rejected = document.getElementById("rejected");
+const allcards = document.getElementById("all-card");
+let jobcount = document.getElementById("job-count");
+// function for btn toggle
+const maincontainer = document.querySelector('main');
+function getStatusBadge(status) {
+    if (status === 'Interview') return '<div class="badge badge-success description rounded-md py-5 px-6 font-bold">Interview</div>';
+    if (status === 'Rejected') return '<div class="badge badge-error description rounded-md py-5 px-6 font-bold">Rejected</div>';
+    return '<div class="badge badge-md bg-primary-content rounded-md py-5 description">Not Applied</div>';
+}
